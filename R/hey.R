02b746fa7f04a7a81c8be2c84d40f7e214e2a2aa
qusai25 <qusai@ucsb.edu>
2020-03-14 18:19:01 -0700

    @param stuff

--- R/hey.R
+++ R/hey.R
@@ -1,1 +1,49 @@
+#' Power Required by Speed
+#'
+#' Description - summary of what your model/function does
+#'
+#'
+#' Tagged (using special “key” words)
+#'
+#' @param inputs/parameter description
+#' @return what your function returns (outputs)
+#' @example how to use it
+#' @references citations or urls
+#' @author YOU
+#'
+#'
+#'
+#'
+#' @param precip
+#' @param v high_precip
+#' @param m vehicle mass (kg)
+#' @param A area of front of vehicle (m2)
+#' @param g acceleration due to gravity (m/s) default=9.8
+#' @param pair (kg/m3) default =1.2
+#' @return power (W)
+#'
+#' inputs/parameter description
+#' @return Flooding Potential
+#' @example You can use this function to determine flood risk in Santa Barbara County using rain and temparature data.
+#' @references citations or urls
+#' @author Bobby Miyashiro and Qusai Bhaijeewala
+
+
+
+
+#' This function determines the power required to keep a vehicle moving at
+#' a given speed
+#' @param cdrag coefficient due to drag default=0.3
+#' @param crolling coefficient due to rolling/friction default=0.015
+#' @param v vehicle speed (m/2)
+#' @param m vehicle mass (kg)
+#' @param A area of front of vehicle (m2)
+#' @param g acceleration due to gravity (m/s) default=9.8
+#' @param pair (kg/m3) default =1.2
+#' @return power (W)
+
+
+
 #heyyyyy
+install.packages("devtools")
+install.packages("roxygen2")
